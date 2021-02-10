3c9a33e78ce3f703c645fc5541230c0262db7413
Aline Lima <aline.lima@neon.com.br>
2021-02-10 02:19:27 -0300

    Novas funcoes implementadas no sistemaCCCadicionada e testada

--- funcoes.php
+++ funcoes.php
@@ -7,4 +7,14 @@ function foo ($arg_1, $arg_2, /* ..., */ $arg_n)
     echo "Exemplo de função.\n";
     return $valor_retornado;
 }
+
+bar();
+
+if ($makefoo) {
+  function foo()
+  {
+    echo "Eu não existo até que o programa passe por aqui.\n";
+  }
+}
+
 ?>
